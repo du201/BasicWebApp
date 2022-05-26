de3d564ec75d2908d95339fee67d56c0d5ad76c2
Xin Du <54476661+du201@users.noreply.github.com>
2022-05-26 16:27:00 -0400

    haha

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -4,7 +4,7 @@ public class QueryProcessor {
 
     public String process(String query) {
         if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+            return "William123123 Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
